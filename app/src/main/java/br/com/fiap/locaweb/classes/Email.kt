56cccca4490d4db04b86739f8a6914a0087e4cd3
Thomas <thomasjefferson.dev@gmail.com>
2024-09-12 21:42:59 -0300

    feat: final creation -> Email.kt and SpamCheck.kt, Attachment.kt create

--- app/src/main/java/br/com/fiap/locaweb/classes/Email.kt
+++ app/src/main/java/br/com/fiap/locaweb/classes/Email.kt
@@ -3,7 +3,7 @@ package br.com.fiap.locaweb.classes
 class Email(
     val sender: String,
     val emailContent: String,
-//    val attachment: List<Attachment>,
+    val attachment: List<Attachment>,
     val link: List<String>
 ){
 
--- app/src/main/java/br/com/fiap/locaweb/methods/SpamCheck.kt
+++ app/src/main/java/br/com/fiap/locaweb/methods/SpamCheck.kt
@@ -1,5 +1,34 @@
 package br.com.fiap.locaweb.methods
 
-//fun isSpam(email: Email): Boolean{
-//
-//}
+import br.com.fiap.locaweb.classes.Attachment
+import br.com.fiap.locaweb.classes.Email
+import br.com.fiap.locaweb.lists.ContactsList
+import br.com.fiap.locaweb.lists.spamKeywords
+import br.com.fiap.locaweb.lists.suspiciousDomains
+import br.com.fiap.locaweb.lists.suspiciousExtensions
+
+fun isSpam(email: Email): Boolean{
+    return containsSpamKeywords(email.emailContent) ||
+            hasSuspiciousAttachments(email.attachment) ||
+            containsSuspiciousLinks(email.emailContent) ||
+            isAContact(email.sender)
+}
+
+fun containsSpamKeywords(content: String): Boolean {
+    return spamKeywords.any { content.contains(it, ignoreCase = true) }
+}
+
+fun hasSuspiciousAttachments(attachments: List<Attachment>): Boolean {
+    return attachments.any { attachment ->
+        suspiciousExtensions.any { attachment.name.endsWith(it, ignoreCase = true) }
+    }
+}
+
+fun containsSuspiciousLinks(content: String): Boolean {
+    return suspiciousDomains.any { content.contains(it, ignoreCase = true) }
+}
+
+fun isAContact(sender: String): Boolean {
+    return !ContactsList.any {sender.contains(it, ignoreCase = true)}
+}
+
